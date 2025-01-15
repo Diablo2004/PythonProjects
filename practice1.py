f011c403e1edf0618f2a8472b1ade53d3d57b897
Diablo2004 <mehulmickey2004@gmail.com>
2025-01-15 15:31:28 +0530

    practice data types

---
+++ practice1.py
@@ -0,0 +1,10 @@
+# Variables and Data Types
+name = "Alice"       # String
+age = 25             # Integer
+height = 5.7         # Float
+is_student = True    # Boolean
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
