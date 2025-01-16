68dd85e16aab482200a6a8cd8a89e647aabc3574
Diablo2004 <mehulmickey2004@gmail.com>
2025-01-16 18:23:11 +0530

    practice arithmetic operations

---
+++ practice8.py
@@ -0,0 +1,9 @@
+a, b = 10, 3
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)  # Float division
+print("Floor Division:", a // b)  # Integer division
+print("Modulus:", a % b)
+print("Exponentiation:", a ** b)
