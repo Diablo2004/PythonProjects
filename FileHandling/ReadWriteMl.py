80ac87177c34288c37c0f0a313e8c99b5bac4157
Diablo2004 <mehulmickey2004@gmail.com>
2025-02-12 16:47:39 +0530

    YAML files

---
+++ FileHandling/ReadWriteMl.py
@@ -0,0 +1,18 @@
+import yaml
+
+with open('one.yaml','r') as read_file:
+    content=yaml.safe_load(read_file)
+    print(content)
+    print(type(content))
+
+with open("data.yaml", "r") as file:
+    existing_data = yaml.safe_load(file)
+
+# Modify data
+existing_data["email"] = "mehul@example.com"
+
+# Write updated YAML back
+with open("data.yaml", "w") as file:
+    yaml.dump(existing_data, file, default_flow_style=False)
+
+print("Updated YAML file!")
