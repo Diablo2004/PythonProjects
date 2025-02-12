754b1fe78d4dbe85092731945877a08db15b4d2e
Diablo2004 <mehulmickey2004@gmail.com>
2025-02-12 16:49:01 +0530

    Implementing Exception Handling using real life example

---
+++ ErrorHandling/ExceptionHandlingImplementation.py
@@ -0,0 +1,26 @@
+def safe_division():
+    while True:
+        try:
+            num1 = float(input("Enter the first number: "))
+            num2 =float(input("Enter the second number: "))
+            result=num1/num2
+            print(f"Result: {result:.2f}")
+            break
+        except ZeroDivisionError:
+            print("Error: Division by zero is not allowed. Try again.")
+
+        except ValueError:
+            print("Error: Please enter valid numeric values.")
+
+        except KeyboardInterrupt:
+            print("\nOperation interrupted by user. Exiting...")
+            break  # Exit on Ctrl+C
+
+        except Exception as e:
+            print(f"Unexpected Error: {e}")
+
+        print("Let's try again!\n")
+
+
+
+safe_division()
