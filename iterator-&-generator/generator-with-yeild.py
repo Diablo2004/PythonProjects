f4d290feba60e05d55eb1a3d03b131712fd6e7e9
Diablo2004 <mehulmickey2004@gmail.com>
2025-02-17 17:43:48 +0530

    Creating Iterators Using iter() and next() Generator Functions with yield Understanding Decorators Working on custom decorator right now

---
+++ iterator-&-generator/generator-with-yeild.py
@@ -0,0 +1,75 @@
+def fibonacci():
+    """
+    Generator that yields the fibonacci sequence
+    """
+    a,b=0,1
+    while True:
+        yield a
+        a,b=b,a+b
+
+fib=fibonacci()
+for _ in range(10): # underscore is used as a variable name when you want to indicate that the value of variable is not important.
+    print(next(fib))
+
+print("-------------")
+
+def even_numbers(start, end):
+    """
+    Generator that yields even numbers in the range from start to end (inclusive).
+    """
+    for num in range(start, end + 1):
+        if num % 2 == 0:
+            yield num
+
+evens = even_numbers(1, 20)
+
+for num in evens:
+    print(num)
+
+print("-------------")
+
+def is_prime(n):
+    """
+    Checks if a number n is prime.
+    """
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def prime_numbers():
+    """
+    Generator that yields prime numbers indefinitely.
+    """
+    num = 2
+    while True:
+        if is_prime(num):
+            yield num
+        num += 1
+
+primes = prime_numbers()
+
+# Get the first 10 primes
+for _ in range(10):
+    print(next(primes))
+
+print("-------------")
+
+def character_count(string):
+    """
+    Generator that counts the occurrence of characters in a string, yielding the running count.
+    """
+    counts = {}
+    for char in string:
+        if char not in counts:
+            counts[char] = 1
+        else:
+            counts[char] += 1
+        yield counts
+
+count_gen = character_count("hello world")
+
+for count in count_gen:
+    print(count)  # Output will show the running count of characters encountered so far.
---
+++ iterator-&-generator/using-iter-&-next.py
@@ -0,0 +1,93 @@
+class CountDown:
+    """
+    Simple countdown iterator class that counts down from a given number.
+    """
+    def __init__(self, start):
+        self.start = start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.start <= 0:
+            raise StopIteration
+        self.start -= 1
+        return self.start
+
+# Create an iterator
+count = CountDown(5)
+iterator = iter(count)
+
+# Use next() to get elements one by one
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+# print(next(iterator)) - Stops Iteration
+
+print("-------------")
+
+# Example: Using iter() and next() on a list
+fruits = ["apple", "banana", "cherry", "date"]
+
+# Create an iterator from the list using iter()
+fruit_iter = iter(fruits)
+
+# Use next() to iterate over the items in the list
+print(next(fruit_iter))
+print(next(fruit_iter))
+print(next(fruit_iter))
+print(next(fruit_iter))
+
+# print(next(fruit_iter))  # Raises StopIteration
+
+print("------------")
+
+# Example: Using iter() and next() on a string
+word = "Python"
+
+# Create an iterator from the string using iter()
+char_iter = iter(word)
+
+# Use next() to get each character
+print(next(char_iter))
+print(next(char_iter))
+print(next(char_iter))
+print(next(char_iter))
+print(next(char_iter))
+print(next(char_iter))
+
+# print(next(char_iter))
+
+print("-----------")
+# Example: Using iter() and next() on a dictionary's keys
+my_dict = {"a": 1, "b": 2, "c": 3}
+
+# Create an iterator for the dictionary keys
+key_iter = iter(my_dict)
+
+# Use next() to get each key in the dictionary
+print(next(key_iter))
+print(next(key_iter))
+print(next(key_iter))
+# print(next(key_iter))
+
+print("--------------")
+
+# Example: Using iter() and next() to read a file
+file = open("sample.txt", "r")
+
+# Create an iterator for the file
+file_iter = iter(file)
+
+# Use next() to read each line
+print(next(file_iter))  # Output: Line 1 content
+print(next(file_iter))  # Output: Line 2 content
+print(next(file_iter))  # Output: Line 3 content
+
+# print(next(file_iter))  # Raises StopIteration
+
+file.close()
+
+
